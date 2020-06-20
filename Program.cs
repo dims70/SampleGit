c3f7e22e86e0db0118fa57f16610e8a42d3e0c8f
Dmitriy Shkarpetov <shkarpetovd@mail.ru>
2020-06-20 15:15:40 +0300

    Тестовый коммит

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
