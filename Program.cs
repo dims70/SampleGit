7dc504cf574bbcaa5b6460202981051dc163d70c
Dmitriy Shkarpetov <shkarpetovd@mail.ru>
2020-06-20 16:06:40 +0300

    Второй тестовый коммит

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace SampleGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = {1,2,3};
+	    Console.Write(string.Join(" ", arr));
         }
     }
 }
